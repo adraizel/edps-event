824ccf631d8be2126f99c30ab65922e77e561268
宮崎龍一 <ryuichi@Ryuichi-MBA.local>
2018-03-20 22:18:29 +0900

    update schema

---
+++ db/schema.rb
@@ -0,0 +1,57 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20180320131504) do
+
+  create_table "event_joins", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_event_joins_on_event_id"
+    t.index ["user_id"], name: "index_event_joins_on_user_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "charge"
+    t.string "roll_call_point"
+    t.string "location"
+    t.datetime "roll_call_time"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "join_limit"
+    t.integer "organizer_id"
+    t.boolean "deleted", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["organizer_id"], name: "index_events_on_organizer_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.string "name", null: false
+    t.string "student_number", null: false
+    t.date "birthday", null: false
+    t.string "allergy_data", default: ""
+    t.string "remark", default: ""
+    t.integer "role", default: 0, null: false
+    t.integer "executive", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+end
