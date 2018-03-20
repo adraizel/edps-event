2349c83c29077a3c45b78c24c44de996a8eb6d70
宮崎龍一 <ryuichi@Ryuichi-MBA.local>
2018-03-20 20:55:41 +0900

    install sorcery

---
+++ db/migrate/20180320113155_sorcery_core.rb
@@ -0,0 +1,21 @@
+class SorceryCore < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :email,            :null => false
+      t.string :crypted_password
+      t.string :salt
+
+      t.string :name, null: false
+      t.string :student_number, null: false
+      t.date :birthday, null: false
+      t.string :allergy_data, default: ""
+      t.string :remark, default: ""
+      t.integer :role, null: false, default: 0
+      t.integer :executive, default: 0
+      
+      t.timestamps                :null => false
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
