d1b008a7bd8a1cee9b53268f5e09bcc8b2daf3f0
宮崎龍一 <ryuichi@Ryuichi-MBA.local>
2018-03-21 20:41:52 +0900

    add jQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+  $(".alert .icon-close").click(function() {
+    $(this).parent().remove();
+  }); 
+ });
