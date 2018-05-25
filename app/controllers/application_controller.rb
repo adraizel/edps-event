1bcf0cb41a9c122468061c1a6f045b43010d558b
M.Ryuichi <drancornus0913@icloud.com>
2018-05-25 12:18:04 +0900

    banken init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  include Banken
   protect_from_forgery with: :exception
+
+  def set_back_url(url)
+    session[:return_to_url] = url
+  end
 end
