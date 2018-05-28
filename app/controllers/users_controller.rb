aec5d8d7820e525061968cad62e7d534548c4d62
M.Ryuichi <drancornus0913@icloud.com>
2018-05-28 21:13:43 +0900

    patch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action ->{
     require_login(user_path)
-  }
+  }, except: [:new, :create]
 
   def show
     @user = User.find(current_user.id)
