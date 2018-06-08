bb34f68527bd5652caabdc54290aeade410c0ced
M.Ryuichi <drancornus0913@icloud.com>
2018-06-08 15:47:44 +0900

    seed修正

--- db/seeds/development/user_events.rb
+++ db/seeds/development/user_events.rb
@@ -1,3 +1,16 @@
+UserEvent.create!([
+  user_id: 1,
+  event_id: 2
+])
+UserEvent.create!([
+  user_id: 1,
+  event_id: 3
+])
+UserEvent.create!([
+  user_id: 1,
+  event_id: 4
+])
+
 num = [3, 4, 5, 6, 7, 8, 9, 10, 11]
 rem = [""] + %w(一時間遅れます 一時間早く帰ります 30分遅れます 二次会とかありますか?)
 1.upto 3 do |t| 
