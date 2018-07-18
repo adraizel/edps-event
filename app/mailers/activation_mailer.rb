434b346896349fd6df58861a6950be85ead68b47
Ryuichi M <wolupila913@outlook.jp>
2018-07-19 02:11:26 +0900

    メール文章の変更

--- app/mailers/activation_mailer.rb
+++ app/mailers/activation_mailer.rb
@@ -1,7 +1,8 @@
 class ActivationMailer < ApplicationMailer
   def activation_needed_email(user)
     @user = user
-    @url  = "http://0.0.0.0:3000/users/activate?token=#{user.activation_token}"
+    @url = "http://0.0.0.0:3000/users/activate?token=#{user.activation_token}"
+    @url = ENV['SITE_URL'] + "/user/activate?=" user.activation_token if ENV['SITE_URL'].present?
     mail(:to => user.email,
          :subject => "Edeventにようこそ")
   end
@@ -9,6 +10,7 @@ class ActivationMailer < ApplicationMailer
   def activation_success_email(user)
     @user = user
     @url  = "http://0.0.0.0:3000/login"
+    @url = ENV['SITE_URL'] + "/login" if ENV['SITE_URL'].present?
     mail(:to => user.email,
          :subject => "アカウント認証が完了しました")
   end
