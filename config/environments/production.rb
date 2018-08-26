ae41d9411244c4f7aca8109ae800b56cf2a29c65
M.Ryuichi <drancornus0913@outlook.jp>
2018-08-27 05:05:35 +0900

    凡ミス修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,11 +64,11 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              ENV[SMTP_ADDR],
-    port:                 ENV[SMTP_PORT],
-    domain:               ENV[SMTP_DOMAIN],
-    user_name:            ENV[SMTP_USERNAME],
-    password:             ENV[SMTP_PASSWORD],
+    address:              ENV['SMTP_ADDR'],
+    port:                 ENV['SMTP_PORT'],
+    domain:               ENV['SMTP_DOMAIN'],
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true  }
 
