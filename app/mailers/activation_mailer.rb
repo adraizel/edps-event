66844ba24ed8cf841c85417275cff8d22c7325e9
Ryuichi M <wolupila913@outlook.jp>
2018-08-27 15:37:22 +0900

    メール内のURL修正

--- app/mailers/activation_mailer.rb
+++ app/mailers/activation_mailer.rb
@@ -1,16 +1,16 @@
 class ActivationMailer < ApplicationMailer
   def activation_needed_email(user)
     @user = user
-    @url = "http://localhost:3000/user/activate?token=#{user.activation_token}"
-    @url = ENV['SITE_URL'] + "/user/activate?=" + user.activation_token if ENV['SITE_URL'].present?
+    @url = ENV['SITE_URL'] || 'http://localhost:3000'
+    @url = @url + '/user/activate?token=' + user.activation_token
     mail(:to => user.email,
          :subject => "Edeventにようこそ")
   end
    
   def activation_success_email(user)
     @user = user
-    @url  = "http://localhost:3000/login"
-    @url = ENV['SITE_URL'] + "/login" if ENV['SITE_URL'].present?
+    @url = ENV['SITE_URL'] || 'http://localhost:3000'
+    @url = @url + '/login'
     mail(:to => user.email,
          :subject => "アカウント認証が完了しました")
   end
