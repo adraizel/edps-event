6467a6fc428acafd26481a0a2bb069bfacc36c73
Ryuichi M <wolupila913@outlook.jp>
2018-10-16 17:45:30 +0900

    スコープの追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -22,32 +22,33 @@ class Event < ApplicationRecord
   has_many :user_events, dependent: :destroy
   has_many :participated_user, through: :user_events, source: :user
 
+  default_scope { where(deleted: false) }
+  
+  scope :after_finishing, -> { where('? > start_time', Date.today) }
+  scope :before_beginning, -> { where('? <= start_time', Date.today) }
+  scope :acceptance_finished, -> { where('join_limit < ?', Date.today) }
+  scope :during_acceptance, -> { where('join_limit >= ?', Date.today) }
+
   validates :title, presence: true
   validates :description, presence: true
   validates :charge, numericality: {only_integer: true, greater_than_or_equal_to: 0}
   validates :location, presence: true
-  validate :checkDate
+  validate :check_date
+  validate :check_date_on_create, on: :create
 
-  def validate_on_create
-    validate :checkDateOnCreate
-  end
-
-  def validate_on_update
-  end
-
-  def checkDate
+  def check_date
     unless join_limit < start_time
-      errors.add(:join_limit,"は#{:start_time}よりも前の日時にしてください")
-      errors.add(:start_time,"は#{:join_limit}よりも後の日時にしてください")
+      errors.add(:join_limit, "は#{:start_time}よりも前の日時にしてください")
+      errors.add(:start_time, "は#{:join_limit}よりも後の日時にしてください")
     end
   end
 
-  def checkDateOnCreate
-    errors.add(:start_time,'は今日以降の日時を指定してください') unless Date.today <= start_time
-    errors.add(:join_limit,'は今日以降の日時を指定してください') unless Date.today <= join_limit
+  def check_date_on_create
+    errors.add(:start_time, 'は今日以降の日時を指定してください') unless Date.today <= start_time
+    errors.add(:join_limit, 'は今日以降の日時を指定してください') unless Date.today <= join_limit
   end
 
   def isJoined?(user)
-    user_events.map{|j|j.user_id}.include?(user.id) if user
+    user_events.map(&:user_id).include?(user.id) if user
   end
 end
